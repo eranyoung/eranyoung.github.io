01985ca282bece9407f41ba65851f60a793008e2
Eran Young <eranyoung1@gmail.com>
2022-02-07 13:53:45 -0800

    DotPlot

--- Assignment2/dotplot.js
+++ Assignment2/dotplot.js
@@ -1,5 +1,5 @@
 function preload() { 
-    table = loadTable("https://gist.githubusercontent.com/eranyoung/4734b5f9296c092c253fc1d07e2f8e8a/raw/da087803f3b107ad2357958cd47cb28d71ab81db/cancer(fullSheet)", "csv", "header");
+    table = loadTable("https://gist.githubusercontent.com/eranyoung/4d6c770200149722d348e1d967760c7c/raw/3ac0bd94cd0e5034dfa7ab358bfcf7b010bb2afa/cancer(rateonlyordered).csv", "csv", "header");
 }
 
 function setup() { 
@@ -19,41 +19,38 @@ function draw() {
     line(80, 10, 80, pageHeight - 10);
     line(50, pageHeight - 50, 2140, pageHeight - 50);
     
-    let max = 0;
+    let max = 300;
 
-    let ppt = (pageHeight - 110) / 300;
+    let ppt = (pageHeight - 110) / 2.3;
 
     let c = 80;
     textSize(14);
     for(let i = 0; i < numberOfRows; i++) { 
-        c+=40;
+        c+=20;
         line(c, pageHeight - 60, c, pageHeight - 40);
         fill(52, 50, 168);
         textAlign(CENTER);
-        text(table.getString(i, 0).substring(0,2).toUpperCase(), c, pageHeight - 20);
+        textSize(12);
+        text(table.getString(i, 0).substring(0,2).toUpperCase(), c, pageHeight - 30);
         let rate = table.getNum(i, 1);
         fill(168, 50, 50);
-        rectMode(CORNERS);
-        let circleHeight = Math.round((pageHeight-50-(rate*ppt))/50) * 50;
-        for(let p = Math.round(circleHeight/75)*75; p < pageHeight-50; p+=75) {
-            circle(c, p, 20, 20); 
-        }
-        if(rate > max) { 
-            max = rate;
-        }
+        let circleHeight = pageHeight-50-rate*(rate/ppt);
+        circle(c, circleHeight, 15, 15); 
+        
     }
 
     max = Math.round(max / 100) * 100;
     for(let n = 1; n <= 6; n++) { 
         line(70, n*75, 90, n*75);
+        textSize(14);
         text(max, 55, n * 75 + 3);
-        max-=50;
+        max-=40;
     }
 
     text("0", 40, pageHeight - 46);
     textSize(20);
     fill(52, 50, 168);
-    text("State", 1100, pageHeight - 5);
+    text("State", 650, pageHeight - 5);
     fill(168, 50, 50);
     rotate(radians(270));
     text("Cancer Rate", -350, 20);
