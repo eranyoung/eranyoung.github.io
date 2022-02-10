8033189e7d9560cfffcb435fa38254c8e86131b5
eranyoung <eranyoung1@gmail.com>
2022-02-10 10:37:29 -0800

    heatmap size change

--- Assignment2/heatmap.js
+++ Assignment2/heatmap.js
@@ -3,7 +3,7 @@ function preload() {
 }
 
 function setup() { 
-    createCanvas(500, 825);
+    createCanvas(600, 1300);
     numberOfRows = table.getRowCount();
     numberOfColumns = table.getColumnCount();
 }
@@ -20,28 +20,38 @@ function draw() {
         textAlign(RIGHT);
         fill(0, 0, 0);
         textSize(13)
-        text(state, 160, r * 75 + 60);
+        text(state, 120, r * 120 + 60);
         for(let c = 0; c < 4; c++) { 
             let value = table.getNum(r, c+1);
             textAlign(LEFT);
             let rgb = getValueBetweenYellowRed(value/max);
             fill(rgb[0], rgb[1] , rgb[2]);
-            rect(c * 75+ 170, r * 75 + 7, 75, 75);
+            rect(c * 120 + 130, r * 120 + 7, 100, 100);
             fill(0,0,0);
             //text("Red: " + rgb[0] + "\nGreen: " + rgb[1] + "\nBlue: " + rgb[2], c * 120 + 140, r * 120 + 30);
             if(r == 9) { 
                 let cName = table.columns[c+1];
                 let columnName = cName.substring(10, cName.length);
                 textAlign(CENTER);
-                text(columnName, c*75+ 210, r*75+95);
+                text(columnName, c*120+180, r*120+130);
             }
         }
     }
 
+    for(let m = 1; m <= 10; m++) {
+        let color = getValueBetweenYellowRed(m/10);
+        fill(color[0], color[1], color[2]);
+        square(m*20, 1250, 20);
+    }
+    fill(0,0,0);
+    textSize(12);
+    text("2", 20, 1290)
+    text("1100", 220, 1290)
+
     textSize(17)
-    text("Age", 310, 800);
+    text("Age", 360, 1240);
     rotate(radians(270));
-    text("State", -400, 30);
+    text("State", -650, 20);
 }
 
 function getValueBetweenYellowRed(value)
